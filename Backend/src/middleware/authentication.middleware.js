1aea9937b80b681280bae9dc9d9451fa19b4c336
Hamza Sajid <hamzakhan174722@gmail.com>
2024-08-17 10:14:07 +0500

    Add files via upload

---
+++ Backend/src/middleware/authentication.middleware.js
@@ -0,0 +1,45 @@
+import { API_Error_handler } from "../utils/api_error_handler.js";
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const verify_jwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.access_token ||
+      req.header("Authorization")?.replace("Bearer ", ""); // here we access the token
+
+    if (!token) {
+      throw new API_Error_handler(401, "Un-Authorized Requst");
+    }
+
+    //  token expire will check by server . it check currect time and the time which is stored in token while creating
+    const decoded_token = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decoded_token?._id).select(
+      "-password -refresh_token"
+    );
+    if (!user) {
+      throw new API_Error_handler(401, "invalid access token");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new API_Error_handler(401, error?.message || "invalid access token");
+  }
+});
+
+export { verify_jwt };
+
+/*
+
+* req.cookies is an object that contains all cookies sent by the client.
+* req.cookies?.access_token retrieves the value of the access_token cookie if it exists.
+* Web browsers typically use cookies to store and send tokens automatically with each request.
+
+
+* Mobile apps, API clients, and other non-browser clients often use the Authorization header to send tokens.
+* Using req.header("Authorization") allows these clients to send the token in the HTTP header.
+ */
--- Backend/src/middleware/multer.middleware.js
+++ Backend/src/middleware/multer.middleware.js
@@ -1,12 +1,12 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
+  destination: function (req, file, cb) {
     cb(null, "./public");
   },
 
-  filename: function (req, res, cb) {
-    cb(nill, file.originalname);
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   },
 });
 
